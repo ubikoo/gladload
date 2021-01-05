e93eb6a531a21b8e48a89aaca45f44334945f2e2
Carlos Correia <ubikoo@mailfence.com>
2021-01-05 13:15:43 +0000

    Import glad files.

---
+++ make.mk
@@ -0,0 +1,10 @@
+MODNAME  := glad
+CXX_SRCS += $(filter-out $(wildcard $(ROOTDIR)/$(MODNAME)/src/_*.cpp), \
+                         $(wildcard $(ROOTDIR)/$(MODNAME)/src/*.cpp))
+CXX_INCS += $(wildcard $(ROOTDIR)/$(MODNAME)/include/*.hpp)
+
+C_SRCS   += $(filter-out $(wildcard $(ROOTDIR)/$(MODNAME)/src/_*.c), \
+                         $(wildcard $(ROOTDIR)/$(MODNAME)/src/*.c))
+C_INCS   += $(wildcard $(ROOTDIR)/$(MODNAME)/include/*.h)
+
+CFLAGS   += -I$(ROOTDIR)/$(MODNAME)/include
