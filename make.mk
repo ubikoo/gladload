7b262221d52f654d6075ed9afd0e220dff97a6fb
Carlos Correia <ubikoo@mailfence.com>
2021-03-28 15:41:58 +0100

    Rename make.mk to makefile.mk

--- make.mk
+++
@@ -1,5 +0,0 @@
-MODNAME  := .
-SOURCES  += $(filter-out $(wildcard $(ROOTDIR)/src/$(MODNAME)/_*.c), \
-                         $(wildcard $(ROOTDIR)/src/$(MODNAME)/*.c))
-INCLUDES += $(wildcard $(ROOTDIR)/include/$(MODNAME)/*.h)
-CFLAGS   += -I$(ROOTDIR)/include
